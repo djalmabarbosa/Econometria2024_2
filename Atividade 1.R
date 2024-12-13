1fe6df9ad56bedf18b3d02679800dc95ce401081
djalmabarbosa <djalma.barboa@ufr.edu.br>
2024-12-13 11:03:38 -0400

    Atualização.

---
+++ Atividade 1.R
@@ -0,0 +1,28 @@
+# Atividade 1
+
+# Na Aula 3 utilizamos o banco de dados *penguins*, que está no pacote *palmerpenguins*,
+# para aprender como podemos gerar visualizações utilizando o pacote *ggplot2*
+
+# No banco de dados penguins, as variáveis estão em inglês. 
+
+# 1) Utilize a função *rename*, que vimos na Aula 2, para modificar os nomes das 
+# variáveis utulizadas na Aula 3.
+# Por exemplo, a variável *body_mass_g* pode passar a se chamar "peso_g*.
+
+# Observe que ao carregar o banco "penguins" o banco de dados está na memória, e não
+# aparece no "Environment".
+
+# Para modificar as variáveis, crie um novo objeto (dados) com o banco de dados 'penguins':
+
+library(palmerpenguins)
+
+dados <- penguins
+
+# Faça as modificações no objeto 'dados', 
+
+# 2) Use o objeto 'dados', com as variáveis em português, para refazer as visualizações que 
+# fizemos na Aula 3.
+
+# Respostas
+
+
